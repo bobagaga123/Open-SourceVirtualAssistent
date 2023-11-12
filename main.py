ad7c45b586b47b053dbcc3221af1eb30d42d587f
BOGDAN <bobagaga123@yandex.ru>
2023-11-12 12:19:33 +0300

    First steps have done

---
+++ main.py
@@ -0,0 +1,22 @@
+import queue
+import sounddevice as sd
+import vosk
+
+q = queue.Queue()
+
+model = vosk.Model("small-ru")
+
+device = sd.default.device = 0, 4
+samplerate = int(sd.query_devices(device[0], 'input')['default_samplerate'])
+
+def callback(indata, frames, time, status):
+    q.put(bytes(indata))
+
+with sd.RawInputStream(samplerate=samplerate, blocksize= 8000, device=device[0], dtype='int16', channels=1, callback=callback):
+    rec = vosk.KaldiRecognizer(model, samplerate)
+    while True:
+        data = q.get()
+        if rec.AcceptWaveform(data):
+            print(rec.Result())
+        else:
+            print(rec.PartialResult())
