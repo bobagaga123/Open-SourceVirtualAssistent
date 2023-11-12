ca1f1f55e4783ab09aa62856d85e22b7a639beee
BOGDAN <bobagaga123@yandex.ru>
2023-11-12 20:46:13 +0300

    Прикрутил загрузку скиллов из директории

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import queue
 import sounddevice as sd
 import vosk
-from skills import base_skills
+from skills import import_all_py_files, execute_skill
 import json
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+
 q = queue.Queue()
 
 model = vosk.Model("small-ru")
-
+skills_counter, skill_names = import_all_py_files(directory="skills")
 device = sd.default.device = 0, 4
 samplerate = int(sd.query_devices(device[0], 'input')['default_samplerate'])
 
@@ -17,23 +18,31 @@ def callback(indata, frames, time, status):
     q.put(bytes(indata))
 
 def recognize(data, vectorizer, clf):
-    called = base_skills.NAME.intersection(data.split())
+    base_skills_NAME = execute_skill("base_skills", "NAME")
+    called = base_skills_NAME.intersection(data.split())
     if not called:
         return
     data.replace(list(called)[0], '')
     text_vector = vectorizer.transform([data]).toarray()[0]
     answer = clf.predict([text_vector])[0]
-
-    print(answer)
+    cathegory = answer.split()[0]
+    funcname = answer.split()[1]
+    print(" ".join(answer.split()[2:]))
+    print(execute_skill(cathegory, funcname))
 
 def main():
+    dataset = {}
+    for i in range(skills_counter):
+        dataset_from_current_skill = execute_skill(skill_names[i],"data_set")
+        dataset.update(dataset_from_current_skill)
+
     vectorizer = CountVectorizer()
-    vectors = vectorizer.fit_transform(list(base_skills.data_set.keys()))
+    vectors = vectorizer.fit_transform(list(dataset.keys()))
 
     clf = LogisticRegression()
-    clf.fit(vectors, list(base_skills.data_set.values()))
+    clf.fit(vectors, list(dataset.values()))
 
-    del base_skills.data_set
+    del dataset
 
     with sd.RawInputStream(samplerate=samplerate, blocksize=8000, device=device[0], dtype='int16', channels=1,
                            callback=callback):
--- skills.py
+++ skills.py
@@ -5,6 +5,8 @@ import importlib.util
 def import_all_py_files(directory="skills"):
     files = [f for f in os.listdir(directory) if f.endswith(".py")]
 
+    skills_counter = 0
+    skill_names = []
     for file in files:
         module_name = os.path.splitext(file)[0]
         spec = importlib.util.spec_from_file_location(module_name, os.path.join(directory, file))
@@ -13,9 +15,17 @@ def import_all_py_files(directory="skills"):
 
         # Добавляем импортированный модуль в словарь глобальных переменных
         globals()[module_name] = module
+        skills_counter +=1
+        skill_names.append(module_name)
 
-def execute_skill(skill_category, skill_name):
-    func = getattr(globals()[skill_category], skill_name)
-    print(func)
+    return skills_counter, skill_names
 
-import_all_py_files(directory="skills")
+def execute_skill(skill_category, skill_name, *args):
+    object = getattr(globals()[skill_category], skill_name)
+    if callable(object):
+        if len(args) > 0:
+            return object(args)
+        else:
+            return object()
+    else:
+        return object
